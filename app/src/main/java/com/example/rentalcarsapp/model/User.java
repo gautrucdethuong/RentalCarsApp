fd54dd8b0cbe1d1373b16538aa9808b353aed83a
gautrucdethuong <57904796+gautrucdethuong@users.noreply.github.com>
2021-06-09 07:49:33 +0700

    Add function Update profile, verify email

--- app/src/main/java/com/example/rentalcarsapp/model/User.java
+++ app/src/main/java/com/example/rentalcarsapp/model/User.java
@@ -8,30 +8,5 @@ package com.example.rentalcarsapp.model;
  */
 
 public class User {
-    String Displayname;
-
-
-    String Email;
-    long createdAt;
-
-    public User(){};
-    public User(String displayname, String email, long createdAt){
-        this.Displayname=displayname;
-        this.Email=email;
-        this.createdAt=createdAt;
-    }
-
-
-    public String getDisplayname() {
-        return Displayname;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public long getCreatedAt() {
-        return createdAt;
-    }
 
 }
