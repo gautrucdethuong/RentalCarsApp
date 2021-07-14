124005e1127d4845333f9013d00dc5622c5f1ae7
Nguyễn Đức Tông <ductong@miennam24h.vn>
2021-07-14 22:58:16 +0700

    Merge branch 'List_Create_Car' of https://github.com/gautrucdethuong/RentalCarsApp into merge-list
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/rentalcarsapp/model/Bill.java
    #	app/src/main/java/com/example/rentalcarsapp/model/Car.java
    #	app/src/main/java/com/example/rentalcarsapp/ui/login/LoginActivity.java
    #	app/src/main/java/com/example/rentalcarsapp/ui/register/RegisterActivity.java
    #	app/src/main/java/com/example/rentalcarsapp/ui/register/RegisterInforActivity.java
    #	app/src/main/res/layout/activity_item_car_homepage.xml
    #	app/src/main/res/layout/activity_listview_car.xml
    #	app/src/main/res/layout/carmanagement_create.xml

--- app/src/main/java/com/example/rentalcarsapp/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/rentalcarsapp/ui/login/LoginActivity.java
@@ -1,7 +1,6 @@
 package com.example.rentalcarsapp.ui.login;
 
 import android.app.ActivityOptions;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,28 +8,32 @@ import android.util.Log;
 import android.util.Pair;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.rentalcarsapp.DashboardActivity;
-import com.example.rentalcarsapp.MainActivity;
 import com.example.rentalcarsapp.R;
 import com.example.rentalcarsapp.dao.AuthenticationDAO;
 import com.example.rentalcarsapp.dao.Callback;
+import com.example.rentalcarsapp.ui.forgot.ForgotPasswordActivity;
 
 import com.example.rentalcarsapp.ui.home.car.RecyclerCarActivity;
 import com.example.rentalcarsapp.ui.register.RegisterActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Source;
+
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -39,15 +42,15 @@ public class LoginActivity extends AppCompatActivity {
     Button mLoginBtn;
     Button mCreateBtn, forgotTextLink;
     ProgressBar progressBar;
-    //FirebaseAuth fAuth;
+    FirebaseAuth fAuth;
     AuthenticationDAO authDao;
     ImageView imglogo;
+    FirebaseFirestore fStore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         authDao = new AuthenticationDAO();
         mEmail = findViewById(R.id.txtEmail);
         mPassword = findViewById(R.id.txtPassword);
@@ -58,19 +61,49 @@ public class LoginActivity extends AppCompatActivity {
         imglogo = findViewById(R.id.logoImage);
         mWelcome = findViewById(R.id.logo_name);
         mSlogan = findViewById(R.id.slogan_name);
+        fAuth = FirebaseAuth.getInstance();
+        fStore = FirebaseFirestore.getInstance();
 
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (fAuth.getCurrentUser() != null) {
+            String userId= fAuth.getCurrentUser().getUid();
 
-        if (user != null) {
+            DocumentReference docRef = fStore.collection("users").document(userId);
+            Source source = Source.CACHE;
+
+            docRef.get(source).addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        // Document found in the offline cache
+                        DocumentSnapshot document = task.getResult();
+
+                        Log.d("TAG", "Cached document data: " + document.getData());
+                        Map<String, Object> user = document.getData();
+                        String role=String.valueOf(user.get("roleName"));
+                        Log.e("roleName",role);
+                        switch (role){
+                            case "Admin":
+                                Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                                break;
+                            case "Customer":
+                                Toast.makeText(LoginActivity.this, "Customer", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(getApplicationContext(), EditProfileActivity.class));
+                                break;
+                            default:
+                                break;
+                        }
+                    } else {
+                        Log.d("TAG", "Cached get failed: ", task.getException());
+                    }
+                }
+            });
             startActivity(new Intent(getApplicationContext(), RecyclerCarActivity.class));
             finish();
         }
-
-
         mCreateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
 
                 Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
                 Pair[] pairs = new Pair[4];
@@ -82,12 +115,17 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent, options.toBundle());
             }
         });
-
-
         forgotTextLink.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent intent = new Intent(LoginActivity.this, ForgotPasswordActivity.class);
+                Pair[] pairs = new Pair[4];
+                pairs[0] = new Pair<View, String>(imglogo, "logo_image");
+                pairs[1] = new Pair<View, String>(mWelcome, "logo_text");
+                pairs[2] = new Pair<View, String>(mSlogan, "logo_signup");
+                pairs[3] = new Pair<View, String>(forgotTextLink, "txt_transaction");
+                ActivityOptions options = ActivityOptions.makeSceneTransitionAnimation(LoginActivity.this, pairs);
+                startActivity(intent, options.toBundle());
 
             }
         });
@@ -109,22 +147,29 @@ public class LoginActivity extends AppCompatActivity {
                 }
 
                 progressBar.setVisibility(View.VISIBLE);
+
                 authDao.loginFirebaseAuthentication(email, password, new Callback() {
                     @Override
-                    public void isLogin(boolean status) {
+                    public void isLogin(String roleName) {
 
-                        if(status) {
-                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
-                            startActivity(new Intent(getApplicationContext(), RecyclerCarActivity.class));
+                        if(!roleName.isEmpty()) {
+                            switch (roleName){
+                                case "Admin":
+                                    Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+                                    break;
+                                case "Customer":
+                                    Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(getApplicationContext(), RecyclerCarActivity.class));
+                                    break;
+                                default:
+                                    break;
+                            }
+
                         }else{
                             Toast.makeText(LoginActivity.this, "Logged in Failed ! ", Toast.LENGTH_SHORT).show();
                             progressBar.setVisibility(View.GONE);
                         }
-
-                    }
-
-                    @Override
-                    public void isLogin(String roleName) {
 
                     }
 
@@ -134,6 +179,20 @@ public class LoginActivity extends AppCompatActivity {
                     }
 
                 });
+                // authenticate the user
+//                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<AuthResult> task) {
+//                        if(task.isSuccessful()){
+//                            Toast.makeText(LoginActivity.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+//                            startActivity(new Intent(getApplicationContext(), DashboardActivity.class));
+//                        }else {
+//                            Toast.makeText(LoginActivity.this, "Logged in Failed ! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+//                            progressBar.setVisibility(View.GONE);
+//                        }
+//
+//                    }
+//                });
 
             }
         });
